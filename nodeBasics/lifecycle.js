0dda0253b885dc1bc26a445a1c1fefb110b7808d
gabriel.gs <gabriel.gs@sidi.org.br>
2022-10-19 16:19:47 -0300

    adjustments on different codes

--- nodeBasics/lifecycle.js
+++ nodeBasics/lifecycle.js
@@ -2,9 +2,9 @@ const http= require('http');
 
 const server= http.createServer((req, res)=>{
     //here it runs forever, since we don't stop the listener
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
     //here it stops and don't log anything
-    //process.exit()
+    process.exit()
 });
 
 server.listen(3000); 
