df912ff4b516d1287b150ed59bda5d3b58574508
GSousaGabriel <gabriel@multcont.com>
2022-10-20 22:43:48 -0300

    automation packages

--- assignments/lesson1/app.js
+++ assignments/lesson1/app.js
@@ -1,6 +1,4 @@
 const http= require('http');
 const routes= require('./routes.js');
 
-const server= http.createServer(routes);
-
-server.listen(3000)
+const server= http.createServer(routes).listen(3000);
