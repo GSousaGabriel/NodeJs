71d3a173c12b8d336cad0337547278c58d7895db
GSousaGabriel <gabriel@multcont.com>
2022-10-22 00:25:06 -0300

    created views

--- expressJs/routes/admin.js
+++ expressJs/routes/admin.js
@@ -1,9 +1,10 @@
+const path= require('path');
 const express= require('express');
 
 const router= express.Router();
 
 router.get('/add-product', (req, res, next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
     //next(); go to the next middleware
 });
 
