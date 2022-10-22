3a322a8c811f8c6cf5f9bb60e78e5640375bc0f7
GSousaGabriel <gabriel@multcont.com>
2022-10-21 23:47:49 -0300

    routing, filter CRUD request, error page and filter path

--- expressJs/app.js
+++ expressJs/app.js
@@ -1,24 +1,19 @@
 const http= require('http');
 const express= require('express');
+const bodyParser= require('body-parser');
+
+const adminRoutes= require('./routes/admin');
+const shopRoutes= require('./routes/shop');
 
 const app= express();
 
-/*app.use('/', (req, res, next)=>{
-    console.log('first middleware');
-    next();//go to the next middleware
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next)=>{
-    return res;
-});*/
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/users', (req, res, next)=>{
-    console.log('first middleware dummy');
-    next();//go to the next middleware
-});
-
-app.use('/', (req, res, next)=>{
-    res.send('<h2>Completed!!</h2>')
-});
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
 
 app.listen(3000);
