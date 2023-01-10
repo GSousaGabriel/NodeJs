1a2723df8099374e3f9df282730051b5d05c7d61
GSousaGabriel <gabriel@multcont.com>
2023-01-09 23:35:57 -0300

    improved sql code with sequelize

--- usingMySql/controllers/admin.js
+++ usingMySql/controllers/admin.js
@@ -13,13 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      return res.redirect('/');
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -28,7 +35,8 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findByPk(prodId)
+  .then( product => {
     if (!product) {
       return res.redirect('/');
     }
@@ -38,7 +46,9 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product
     });
-  });
+  }).catch(error=>{
+    console.log(error);
+  })
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -47,29 +57,44 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product=>{
+    product.title= updatedTitle;
+    product.price= updatedPrice;
+    product.description= updatedImageUrl;
+    product.imageUrl= updatedDesc;
+    return product.save();
+  })
+  .then(result=>{
+    res.redirect('/admin/products');
+  }).catch(error=>{
+    console.log(error)
+  })
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product
+  .findAll()
+  .then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
-    });
+    })
+  }).catch(error=> {
+    console.log(error);
   });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId).then(product=>{
+    product.destroy();
+  })
+  .then(result=>{
+    res.redirect('/admin/products');
+  })
+  .catch(error=>{
+    console.log(error);
+  });
 };
--- usingMySql/util/database.js
+++ usingMySql/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize= require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '123juca49'
+const sequelize= new Sequelize('node-complete', 'root', '123juca49', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports= sequelize;
