a7cadfa9d7e5dedd0989a961f1b8beb90233e42e
GSousaGabriel <gabriel@multcont.com>
2023-02-09 22:26:45 -0300

    added field validation

--- usingMongoose/controllers/auth.js
+++ usingMongoose/controllers/auth.js
@@ -8,89 +8,65 @@ const transporter = nodeMailer.createTransport(sendGridTransport({
     api_key: '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'
   }
 }))
+const { validationResult } = require('express-validator/check')
 
 exports.getRegister = (req, res, next) => {
   res.render('general/register', {
     pageTitle: 'Register',
     path: '/register',
     message: req.flash('error')
-  });
-};
-
-exports.postRegister = (req, res, next) => {
-  User.find({ email: req.body.email })
-    .then(user => {
-      if (user.length > 0) {
-        return user
-      }
-      return crypt.hash(req.body.pass, 12)
-    })
-    .then(data => {
-      if (typeof data === 'string') {
-        let newUser = new User({ name: req.body.username, email: req.body.email, pass: data })
-        newUser.save()
-          .then(data => {
-            res.redirect('/')
-            return transporter.sendMail({
-              to: req.body.email,
-              from: 'pasteu008@gmail.com',
-              subject: "registered!",
-              html: '<h1> Successfully signed!</h1>'
-            })
-          })
-          .catch(error => {
-            console.log(error)
-          });
-      } else {
-        req.flash('error', 'email already registered!')
-        res.redirect('/register')
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    });
-};
+  })
+}
 
 exports.getIndex = (req, res, next) => {
   res.render('general/index', {
     pageTitle: 'Login',
     path: '/',
     message: req.flash('error')
-  });
-};
+  })
+}
 
 exports.postLogin = (req, res, next) => {
   let email = req.body.email
   let user = ''
-
-  User.findOne({ email })
-    .then(userTry => {
-      if (userTry) {
-        user = userTry
-        return crypt.compare(req.body.pass, userTry.pass)
-      }
+  const errors= validationResult(req)
+  
+  if (!errors.isEmpty()) {
+    return res.status(422).render('general/index', {
+      pageTitle: 'Login',
+      path: '/',
+      message: errors.errors[0].msg
     })
-    .then(isCorrect => {
-      if (!isCorrect) {
-        req.flash('error', 'Invalid email or password')
-        res.redirect('/');
-      } else {
-        req.session.user = user
-        res.redirect('/products');
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    });
-};
+  } else {
+    User.findOne({ email })
+      .then(userTry => {
+        if (userTry) {
+          user = userTry
+          return crypt.compare(req.body.pass, userTry.pass)
+        }
+      })
+      .then(isCorrect => {
+        if (!isCorrect) {
+          req.flash('error', 'Invalid email or password')
+          res.redirect('/')
+        } else {
+          req.session.user = user
+          res.redirect('/products')
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
 
 exports.getRecover = (req, res, next) => {
   res.render('general/recover', {
     pageTitle: 'Recover',
     path: '/recover',
     message: req.flash('error')
-  });
-};
+  })
+}
 
 exports.postRecover = (req, res, next) => {
   crypo.randomBytes(32, (err, buffer) => {
@@ -110,7 +86,7 @@ exports.postRecover = (req, res, next) => {
             res.redirect('/recover')
           }
         })
-        .then(user=>{
+        .then(user => {
           res.redirect('/')
 
           return transporter.sendMail({
@@ -122,10 +98,10 @@ exports.postRecover = (req, res, next) => {
         })
         .catch(error => {
           console.log(error)
-        });
+        })
     }
   })
-};
+}
 
 exports.getReset = (req, res, next) => {
   const resetToken = req.params.token
@@ -138,38 +114,40 @@ exports.getReset = (req, res, next) => {
         resetToken,
         userId: user._id.toString(),
         message: req.flash('error')
-      });
+      })
     })
     .catch(error => {
       console.log(error)
-    });
-};
+    })
+}
 
 exports.postReset = (req, res, next) => {
   let userId = req.body.userId
   let resetToken = req.body.resetToken
   crypt.hash(req.body.pass, 12)
     .then(pass => {
-      return User.updateOne({ _id: userId, resetTokenExp:{$gt: Date.now()}, resetToken }, { pass, resetToken: null, resetTokenExp: null })
+      return User.updateOne({ _id: userId, resetTokenExp: { $gt: Date.now() }, resetToken }, { pass, resetToken: null, resetTokenExp: null })
     })
     .then(user => {
-        res.redirect('/')
+      res.redirect('/')
     })
     .catch(error => {
       console.log(error)
-    });
-};
+    })
+}
 
 exports.postRegister = (req, res, next) => {
-  User.find({ email: req.body.email })
-    .then(user => {
-      if (user.length > 0) {
-        return user
-      }
-      return crypt.hash(req.body.pass, 12)
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('general/register', {
+      pageTitle: 'Register',
+      path: '/register',
+      message: errors.errors[0].msg
     })
-    .then(data => {
-      if (typeof data === 'string') {
+  } else {
+    crypt.hash(req.body.pass, 12)
+      .then(data => {
         let newUser = new User({ name: req.body.username, email: req.body.email, pass: data })
         newUser.save()
           .then(data => {
@@ -183,19 +161,16 @@ exports.postRegister = (req, res, next) => {
           })
           .catch(error => {
             console.log(error)
-          });
-      } else {
-        req.flash('error', 'email already registered!')
-        res.redirect('/register')
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    });
-};
+          })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
-    res.redirect('/');
+    res.redirect('/')
   })
-};
+}
--- usingMongoose/routes/auth.js
+++ usingMongoose/routes/auth.js
@@ -1,27 +1,47 @@
-const express = require('express');
+const express = require('express')
+const authController = require('../controllers/auth')
+const router = express.Router()
+const { check, body } = require('express-validator/check')
+const User = require('../models/user')
 
-const authController = require('../controllers/auth');
 
-const router = express.Router();
+router.get('/register', authController.getRegister)
 
-router.get('/register', authController.getRegister);
+router.post('/register',
+    [
+        check('email').isEmail().withMessage('Invalid Email!').custom((value, { req }) => {
+            // if (value === 'test@test.com') {
+            //     throw new Error('forbitten email!')
+            // } else {
+            //     return true
+            // }
+            return User.find({ email: req.body.email })
+                .then(user => {
+                    if (user.length > 0) {
+                        return Promise.reject('email already registered!')
+                    }
+                })
+        }),
+        body('pass', 'very wrong and general error').isLength({ min: 5, max: 50 }).isAlphanumeric()
+    ], authController.postRegister)
 
-router.post('/register', authController.postRegister);
+router.get('/', authController.getIndex)
 
-router.get('/', authController.getIndex);
+router.get('/login', authController.getIndex)
 
-router.get('/login', authController.getIndex);
+router.post('/login', [
+    check('pass', 'wrong pass format!').isLength({min: 5, max:50}).isAlphanumeric(),
+    body('email').isEmail().withMessage('wrong email format!')
+], authController.postLogin)
 
-router.post('/login', authController.postLogin);
+router.post('/logout', authController.postLogout)
 
-router.post('/logout', authController.postLogout);
+router.get('/recover', authController.getRecover)
 
-router.get('/recover', authController.getRecover);
+router.post('/recover', authController.postRecover)
 
-router.post('/recover', authController.postRecover);
+router.get('/reset/:token', authController.getReset)
 
-router.get('/reset/:token', authController.getReset);
+router.post('/reset', authController.postReset)
 
-router.post('/reset', authController.postReset);
-
-module.exports = router;
+module.exports = router
